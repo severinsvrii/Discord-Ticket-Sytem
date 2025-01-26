ceea626590c075d35d87abf4fc7749162a3e274e
severinsvrii <despiking1@gmail.com>
2025-01-26 01:39:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
     modmail: {
-        ownerID: '', // ← Insert the Server Owner ID.
-        supportId: '', // ← Insert General Support Category ID.
-        premiumId: '', // ← Insert Premium Support Category ID.
-        whitelist: '', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
-        staff: '', // ← Insert Staff Role to be pinged upon ticket creation.
+        ownerID: '1279158875269894198', // ← Insert the Server Owner ID.
+        supportId: '1332871685128454145', // ← Insert General Support Category ID.
+        premiumId: '1332871720469659738', // ← Insert Premium Support Category ID.
+        whitelist: '1332871874635497533', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        staff: '1332872228349542563', // ← Insert Staff Role to be pinged upon ticket creation.
     },
     logs: {
         logschannel: '' // ← Insert Ticket Logs Channel ID.
